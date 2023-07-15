c43a257916d06822eb259bb5614b3b6dcc5de441
Tiziano <tizianocitro98@gmail.com>
2023-07-15 09:54:03 +0000

    Added chart types

---
+++ cc-data/webapp/src/types/charts.ts
@@ -0,0 +1,32 @@
+import {ChartType} from 'src/components/backstage/widgets/widget_types';
+
+export type ChartData = {
+    chartType: ChartType.SimpleLine;
+    lineData: SimpleLineChartData[];
+    lineColor: LineColor;
+} | {
+    chartType: ChartType.NoChart;
+};
+
+export type SimpleLineChartData = {
+    label: string;
+    [key: string]: number | string;
+};
+
+export type LineColor = {
+    [key: string]: string;
+};
+
+export type LineDot = {
+    x: number;
+    y: number;
+    label: string;
+    value: number;
+};
+
+export const defaultDot: LineDot = {
+    x: 0.0,
+    y: 0.0,
+    label: '',
+    value: 0.0,
+};
