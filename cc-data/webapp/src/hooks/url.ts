f08370b82579d108d2bb2329780ad120e8cdd78b
Tiziano <tizianocitro98@gmail.com>
2023-08-03 11:35:38 +0000

    Added url hash validator

--- cc-data/webapp/src/hooks/url.ts
+++ cc-data/webapp/src/hooks/url.ts
@@ -1,6 +1,20 @@
 import {getSiteUrl} from 'src/clients';
 import {PARENT_ID_PARAM, SECTION_ID_PARAM} from 'src/constants';
 
+export const isUrlHashValid = (urlHash: string, all: string[], some: string[]): boolean => {
+    let valid = false;
+    if (!urlHash || urlHash.length < 1) {
+        return valid;
+    }
+    if (all.length > 0) {
+        valid = all.every((a) => urlHash.includes(a));
+    }
+    if (some.length > 0) {
+        valid = some.some((s) => urlHash.includes(s));
+    }
+    return valid;
+};
+
 export const isUrlEqualWithoutQueryParams = (url: string) => {
     const currentUrlWithoutQueryParams = window.location.href.split('?')[0];
     return currentUrlWithoutQueryParams === url || `${currentUrlWithoutQueryParams}/` === url;
