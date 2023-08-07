3348d7a6f1b9e14ae5f16010331c3af8df7992da
Tiziano <tizianocitro98@gmail.com>
2023-08-07 10:15:23 +0000

    Updated url hashing hooks to add telemetry

--- cc-data/webapp/src/hooks/browser.ts
+++ cc-data/webapp/src/hooks/browser.ts
@@ -1,7 +1,13 @@
-import {getCurrentChannelId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/common';
 import {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import {useLocation} from 'react-router-dom';
+import {getCurrentChannelId, getCurrentUser, getCurrentUserId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/common';
+import {getCurrentTeamId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/teams';
+
+import {saveUrlHashTelemetry} from 'src/clients';
+import {channelNameSelector, teamNameSelector} from 'src/selectors';
+
+import {isUrlHashValid} from './url';
 
 export const useUrlHash = (): string => {
     const {hash: urlHash} = useLocation();
@@ -10,7 +16,7 @@ export const useUrlHash = (): string => {
     return renderHash;
 };
 
-export const useCleanHash = () => {
+export const useCleanUrlHash = () => {
     useEffect(() => {
         const hash = localStorage.getItem('previousHash');
         if (!hash) {
@@ -24,15 +30,47 @@ export const useCleanHash = () => {
     });
 };
 
+export const useCleanUrlHashOnChannelChange = () => {
+    const channelId = useSelector(getCurrentChannelId);
+    useEffect(() => {
+        localStorage.setItem('previousHash', '');
+    }, [channelId]);
+};
+
+export const useSaveUrlHashTelemetry = (hash: string) => {
+    const channelId = useSelector(getCurrentChannelId);
+    const channel = useSelector(channelNameSelector(channelId));
+    const teamId = useSelector(getCurrentTeamId);
+    const team = useSelector(teamNameSelector(teamId));
+    const userId = useSelector(getCurrentUserId);
+    const user = useSelector(getCurrentUser);
+
+    useEffect(() => {
+        if (!isUrlHashValid(hash, [], [])) {
+            return;
+        }
+        async function saveUrlHashTelemetryAsync() {
+            saveUrlHashTelemetry({
+                channelId,
+                channelName: channel?.display_name || '',
+                teamId,
+                teamName: team?.display_name || '',
+                userId,
+                username: user?.username || '',
+                urlHash: hash,
+            });
+        }
+        saveUrlHashTelemetryAsync();
+    }, [hash]);
+};
+
 type ScrollIntoViewPositions = {
     block?: ScrollLogicalPosition;
     inline?: ScrollLogicalPosition;
 };
 
 export const useScrollIntoView = (hash: string, positions?: ScrollIntoViewPositions) => {
-    const channelId = useSelector(getCurrentChannelId);
-
-    useCleanHash();
+    useCleanUrlHash();
 
     // When first loading the page, the element with the ID corresponding to the URL
     // hash is not mounted, so the browser fails to automatically scroll to such section.
@@ -53,9 +91,8 @@ export const useScrollIntoView = (hash: string, positions?: ScrollIntoViewPositi
         }
     }, [hash]);
 
-    useEffect(() => {
-        localStorage.setItem('previousHash', '');
-    }, [channelId]);
+    useCleanUrlHashOnChannelChange();
+    useSaveUrlHashTelemetry(hash);
 };
 
 // Doc: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
