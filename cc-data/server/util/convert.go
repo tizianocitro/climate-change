44022a9bfa6b7ee429cd7087cc67f643b4e2d938
Tiziano <tizianocitro98@gmail.com>
2023-08-08 15:16:40 +0000

    Added utility function to extract keys from struct

--- cc-data/server/util/convert.go
+++ cc-data/server/util/convert.go
@@ -1,6 +1,9 @@
 package util
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"reflect"
+)
 
 func Convert(a, b interface{}) error {
 	js, err := json.Marshal(a)
@@ -8,4 +11,21 @@ func Convert(a, b interface{}) error {
 		return err
 	}
 	return json.Unmarshal(js, b)
+}
+
+func ConvertStructToKeys(s interface{}) []string {
+	var keys []string
+
+	// Get the type of the struct
+	structType := reflect.TypeOf(s)
+
+	// Loop through the fields of the struct
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+
+		// Append the field name to the keys slice
+		keys = append(keys, field.Name)
+	}
+
+	return keys
 }
